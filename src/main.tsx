fa47a0bc18595a70518a5cf9bc7af1782b18183f
AldieryGonzalez <aldieryrgon@gmail.com>
2023-10-14 03:59:25 -0500

    init supabase auth

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+import { createClient } from "@supabase/supabase-js";
+import { SessionContextProvider } from "@supabase/auth-helpers-react";
+
+const supabase = createClient(
+	"https://kawwnybalhujdmtujcgo.supabase.co",
+	"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imthd3dueWJhbGh1amRtdHVqY2dvIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTcyNzIzODksImV4cCI6MjAxMjg0ODM4OX0.iseFC6a7zlbjjoLP59jA2sjK3r9P31uIz9gVAwTmOTY"
+);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+	<React.StrictMode>
+		<SessionContextProvider supabaseClient={supabase}>
+			<App />
+		</SessionContextProvider>
+	</React.StrictMode>
+);
