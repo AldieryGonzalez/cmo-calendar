ef821653e57302010b03cf86f3f3f6b8d3a1527a
AldieryGonzalez <aldieryrgon@gmail.com>
2023-10-16 01:38:59 -0500

    Shift Dashboard 50%

--- src/pages/Schedule.tsx
+++ src/pages/Schedule.tsx
@@ -1,18 +1,43 @@
 import { DatePickerWithRange } from "@/components/DateRangePicker";
+import SchedOverview from "@/components/SchedOverview";
+import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { useAuth } from "@/utilities/useAuth";
+import axios from "axios";
 import { addDays } from "date-fns";
 import React from "react";
 import { DateRange } from "react-day-picker";
+import { useQuery } from "react-query";
 
 const Schedule = () => {
+	const { session, isLoading: sessionLoading } = useAuth();
 	const [date, setDate] = React.useState<DateRange | undefined>({
 		from: new Date(),
 		to: addDays(new Date(), 14),
 	});
 
+	const { data } = useQuery("calQuery", fetchCalendar, {
+		enabled: !!session,
+	});
+
+	async function fetchCalendar() {
+		const today = new Date().toISOString();
+		const response = await axios.get(
+			`https://www.googleapis.com/calendar/v3/calendars/hi538hiha983mftk127v1q7mco@group.calendar.google.com/events?timeMin=${today}&orderBy=startTime&singleEvents=true`,
+			{
+				headers: {
+					Authorization: `Bearer ${session?.provider_token}`,
+				},
+			}
+		);
+		return response.data;
+	}
+
 	const handleDateChange = (dateRange: DateRange | undefined) => {
 		setDate(dateRange);
 		console.log(dateRange);
 	};
+
+	if (sessionLoading || !data) return <h1>Loading ...</h1>;
 
 	return (
 		<div className='flex-1 space-y-4 p-8 pt-6'>
@@ -25,6 +50,21 @@ const Schedule = () => {
 					/>
 				</div>
 			</div>
+			<Tabs defaultValue='overview' className='space-y-4'>
+				<TabsList>
+					<TabsTrigger value='overview'>Overview</TabsTrigger>
+					<TabsTrigger value='analytics' disabled>
+						Analytics
+					</TabsTrigger>
+					<TabsTrigger value='reports' disabled>
+						Reports
+					</TabsTrigger>
+					<TabsTrigger value='notifications' disabled>
+						Notifications
+					</TabsTrigger>
+				</TabsList>
+				<SchedOverview shifts={data.items} date={date} />
+			</Tabs>
 		</div>
 	);
 };
